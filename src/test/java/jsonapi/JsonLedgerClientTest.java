b415c9f49634c250686178146c9c55dc9436104d
Gabor Hosszu <gabor@digitalasset.com>
2020-02-05 10:58:27 +0100

    Fix non-compiling test

--- src/test/java/jsonapi/JsonLedgerClientTest.java
+++ src/test/java/jsonapi/JsonLedgerClientTest.java
@@ -4,22 +4,21 @@
  */
 package jsonapi;
 
-import static org.hamcrest.CoreMatchers.containsString;
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.not;
-import static org.junit.Assert.assertThat;
+import org.junit.Ignore;
+import org.junit.Test;
 
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutionException;
-import org.junit.Ignore;
-import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.assertThat;
 
 public class JsonLedgerClientTest {
 
   @Ignore
   @Test
   public void getActiveContracts() throws InterruptedException, ExecutionException {
-    var ledger = new JsonLedgerClient();
+    var ledger = new JsonLedgerClient(null);
     var result = ledger.getActiveContracts().get();
     assertThat(result.statusCode(), is(200));
     assertThat(result.body(), not(containsString("\"result\":[]")));
@@ -28,7 +27,7 @@ public class JsonLedgerClientTest {
   @Ignore
   @Test
   public void webSocket() throws InterruptedException {
-    var ledger = new JsonLedgerClient();
+    var ledger = new JsonLedgerClient(null);
     var latch = new CountDownLatch(1);
     ledger.getActiveContractsViaWebSockets(latch);
     latch.await();
