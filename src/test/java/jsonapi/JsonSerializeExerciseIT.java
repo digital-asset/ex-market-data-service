1a8c2a5622b747b35eddcdd77c4ad776dc5a5f4c
Richard Kapolnai <richard.kapolnai@digitalasset.com>
2020-02-07 14:03:17 +0100

    fix after merge

--- src/test/java/jsonapi/JsonSerializeExerciseIT.java
+++ src/test/java/jsonapi/JsonSerializeExerciseIT.java
@@ -12,48 +12,87 @@ import com.digitalasset.refapps.marketdataservice.extensions.RelTime;
 import com.digitalasset.testing.junit4.Sandbox;
 import com.digitalasset.testing.ledger.DefaultLedgerAdapter;
 import com.digitalasset.testing.utils.ContractWithId;
+import com.fasterxml.jackson.databind.JsonSerializer;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.protobuf.InvalidProtocolBufferException;
 import da.timeservice.timeservice.CurrentTime;
 import da.timeservice.timeservice.TimeConfiguration;
 import da.timeservice.timeservice.TimeManager;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.security.Key;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import jsonapi.apache.ApacheHttpClient;
 import jsonapi.gson.ExerciseCommandSerializer;
 import jsonapi.gson.IdentifierSerializer;
+import jsonapi.gson.InstantSerializer;
 import jsonapi.gson.RecordSerializer;
+import jsonapi.http.Api;
+import jsonapi.http.HttpClient;
+import jsonapi.http.HttpResponse;
+import jsonapi.http.WebSocketClient;
+import jsonapi.http.WebSocketResponse;
+import jsonapi.tyrus.TyrusWebSocketClient;
 import org.junit.*;
 import org.junit.rules.ExternalResource;
 import org.junit.rules.RuleChain;
 import org.junit.rules.TestRule;
 
 public class JsonSerializeExerciseIT {
+
   private static final Path RELATIVE_DAR_PATH = Paths.get("target/market-data-service.dar");
   private static final Party OPERATOR = new Party("Operator");
 
   private static final Sandbox sandbox =
       Sandbox.builder()
           .dar(RELATIVE_DAR_PATH)
-          .useWallclockTime()
           .parties(OPERATOR.getValue())
+          .useWallclockTime()
           .build();
 
   @ClassRule public static ExternalResource startSandbox = sandbox.getClassRule();
 
   @Rule
-  public TestRule processes =
+  public final TestRule processes =
       RuleChain.outerRule(sandbox.getRule()).around(new JsonApi(sandbox::getSandboxPort));
 
+  private final Gson json =
+      new GsonBuilder()
+          .registerTypeAdapter(Identifier.class, new IdentifierSerializer())
+          .registerTypeAdapter(Instant.class, new InstantSerializer())
+          .registerTypeAdapter(Record.class, new RecordSerializer())
+          .registerTypeAdapter(ExerciseCommand.class, new ExerciseCommandSerializer())
+          .create();
+
   private DefaultLedgerAdapter ledger;
+  private HttpClient httpClient;
+  private WebSocketClient webSocketClient;
+  private Api api;
 
   @Before
   public void setUp() {
     ledger = sandbox.getLedgerAdapter();
+    Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+    Map<String, Object> claim = new HashMap<>();
+    claim.put("ledgerId", sandbox.getClient().getLedgerId());
+    claim.put("applicationId", "market-data-service");
+    claim.put("actAs", Collections.singletonList("Operator"));
+    Map<String, Object> claims = Collections.singletonMap("https://daml.com/ledger-api", claim);
+    String jwt = Jwts.builder().setClaims(claims).signWith(key).compact();
+    httpClient = new ApacheHttpClient(this::fromJson, this::toJson, jwt);
+    webSocketClient = new TyrusWebSocketClient(this::fromJsonWs, this::toJson, jwt);
+    api = new Api("localhost", 7575);
   }
 
   @Test
@@ -64,7 +103,7 @@ public class JsonSerializeExerciseIT {
     ContractWithId<TimeManager.ContractId> timeManagerWithId =
         ledger.getMatchedContract(OPERATOR, TimeManager.TEMPLATE_ID, TimeManager.ContractId::new);
     JsonLedgerClient jsonLedgerClient =
-        new JsonLedgerClient(sandbox.getClient().getLedgerId(), this::toJson);
+        new JsonLedgerClient(httpClient, webSocketClient, this::toJson, api);
     jsonLedgerClient.exerciseChoice(timeManagerWithId.contractId.exerciseAdvanceCurrentTime());
     getNextCurrentTime();
     CurrentTime currentTime = getNextCurrentTime();
@@ -90,12 +129,14 @@ public class JsonSerializeExerciseIT {
   }
 
   private String toJson(Object o) {
-    final Gson gson =
-        new GsonBuilder()
-            .registerTypeAdapter(ExerciseCommand.class, new ExerciseCommandSerializer())
-            .registerTypeAdapter(Identifier.class, new IdentifierSerializer())
-            .registerTypeAdapter(Record.class, new RecordSerializer())
-            .create();
-    return gson.toJson(o);
+    return json.toJson(o);
+  }
+
+  private HttpResponse fromJson(InputStream is) {
+    return json.fromJson(new InputStreamReader(is), HttpResponse.class);
+  }
+
+  private WebSocketResponse fromJsonWs(InputStream is) {
+    return json.fromJson(new InputStreamReader(is), WebSocketResponse.class);
   }
 }
