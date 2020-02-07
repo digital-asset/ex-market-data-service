cfa9ba0687c9d0a22f6cc44ff42c34ce9e4fe511
Richard Kapolnai <richard.kapolnai@digitalasset.com>
2020-02-07 12:56:33 +0100

    finished working test for ExerciseCommand conversion

--- src/main/java/jsonapi/gson/IdentifierSerializer.java
+++ src/main/java/jsonapi/gson/IdentifierSerializer.java
@@ -1,4 +1,17 @@
 package jsonapi.gson;
 
-public class IdentifierSerializer {
+import com.daml.ledger.javaapi.data.Identifier;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
+import java.lang.reflect.Type;
+
+public class IdentifierSerializer implements JsonSerializer<Identifier> {
+
+  @Override
+  public JsonElement serialize(
+      Identifier identifier, Type type, JsonSerializationContext jsonSerializationContext) {
+    return new JsonPrimitive(identifier.getModuleName() + ":" + identifier.getEntityName());
+  }
 }
--- src/test/java/jsonapi/JsonSerializeExerciseIT.java
+++ src/test/java/jsonapi/JsonSerializeExerciseIT.java
@@ -8,46 +8,38 @@ import com.daml.ledger.javaapi.data.ExerciseCommand;
 import com.daml.ledger.javaapi.data.Identifier;
 import com.daml.ledger.javaapi.data.Party;
 import com.daml.ledger.javaapi.data.Record;
-import com.digitalasset.refapps.marketdataservice.Main;
+import com.digitalasset.refapps.marketdataservice.extensions.RelTime;
 import com.digitalasset.testing.junit4.Sandbox;
 import com.digitalasset.testing.ledger.DefaultLedgerAdapter;
 import com.digitalasset.testing.utils.ContractWithId;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonParser;
+import com.google.protobuf.InvalidProtocolBufferException;
 import da.timeservice.timeservice.CurrentTime;
+import da.timeservice.timeservice.TimeConfiguration;
+import da.timeservice.timeservice.TimeManager;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.Duration;
 import java.time.Instant;
 import java.util.Collections;
-
-import da.timeservice.timeservice.TimeManager;
-import jsonapi.apache.ApacheHttpClientIT;
 import jsonapi.gson.ExerciseCommandSerializer;
+import jsonapi.gson.IdentifierSerializer;
 import jsonapi.gson.RecordSerializer;
-import org.junit.Before;
-import org.junit.ClassRule;
-import org.junit.Rule;
-import org.junit.Test;
+import org.junit.*;
 import org.junit.rules.ExternalResource;
 import org.junit.rules.RuleChain;
 import org.junit.rules.TestRule;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class JsonSerializeExerciseIT {
-  private final Logger logger = LoggerFactory.getLogger(getClass());
-
   private static final Path RELATIVE_DAR_PATH = Paths.get("target/market-data-service.dar");
   private static final Party OPERATOR = new Party("Operator");
 
   private static final Sandbox sandbox =
       Sandbox.builder()
           .dar(RELATIVE_DAR_PATH)
-          .module("Test.DA.TimeService.TimeServiceTest")
-          .scenario("setupTimeTestWithAlice")
+          .useWallclockTime()
           .parties(OPERATOR.getValue())
           .build();
 
@@ -66,39 +58,44 @@ public class JsonSerializeExerciseIT {
 
   @Test
   public void AdvanceCurrentTimeIsCompletedByTheLedger() throws IOException {
-    logger.info("start");
+    Instant startTime = Instant.parse("2020-02-04T22:57:29Z");
+    Duration modelPeriodTime = Duration.ofHours(1);
+    setupTimeContracts(startTime, modelPeriodTime);
     ContractWithId<TimeManager.ContractId> timeManagerWithId =
-            ledger.getMatchedContract(OPERATOR, TimeManager.TEMPLATE_ID, TimeManager.ContractId::new);
-    logger.info("got manager");
-    JsonLedgerClient jsonLedgerClient = new JsonLedgerClient(sandbox.getClient().getLedgerId(), this::toJson);
+        ledger.getMatchedContract(OPERATOR, TimeManager.TEMPLATE_ID, TimeManager.ContractId::new);
+    JsonLedgerClient jsonLedgerClient =
+        new JsonLedgerClient(sandbox.getClient().getLedgerId(), this::toJson);
+    jsonLedgerClient.exerciseChoice(timeManagerWithId.contractId.exerciseAdvanceCurrentTime());
+    getNextCurrentTime();
+    CurrentTime currentTime = getNextCurrentTime();
+    Assert.assertEquals(startTime.plus(modelPeriodTime), currentTime.currentTime);
+  }
 
-    jsonLedgerClient.exerciseChoice(
-                    timeManagerWithId.contractId.exerciseAdvanceCurrentTime());
-    logger.info("exercised");
-
+  private CurrentTime getNextCurrentTime() {
     ContractWithId<CurrentTime.ContractId> currentTimeWithId =
-            ledger.getMatchedContract(OPERATOR, CurrentTime.TEMPLATE_ID, CurrentTime.ContractId::new);
-    logger.info("got time");
-    CurrentTime currentTime = CurrentTime.fromValue(currentTimeWithId.record);
-    logger.info(currentTime.toString());
+        ledger.getMatchedContract(OPERATOR, CurrentTime.TEMPLATE_ID, CurrentTime.ContractId::new);
+    return CurrentTime.fromValue(currentTimeWithId.record);
+  }
 
-    String result = jsonLedgerClient.getActiveContracts();
-    logger.info(jsonFormat(result));
+  private void setupTimeContracts(Instant startTime, Duration modelPeriodTime)
+      throws InvalidProtocolBufferException {
+    CurrentTime currentTime =
+        new CurrentTime(OPERATOR.getValue(), startTime, Collections.emptyList());
+    ledger.createContract(OPERATOR, CurrentTime.TEMPLATE_ID, currentTime.toValue());
+    TimeConfiguration timeConfiguration =
+        new TimeConfiguration(OPERATOR.getValue(), true, RelTime.fromDuration(modelPeriodTime));
+    ledger.createContract(OPERATOR, TimeConfiguration.TEMPLATE_ID, timeConfiguration.toValue());
+    TimeManager timeManager = new TimeManager(OPERATOR.getValue());
+    ledger.createContract(OPERATOR, TimeManager.TEMPLATE_ID, timeManager.toValue());
   }
 
   private String toJson(Object o) {
     final Gson gson =
-            new GsonBuilder()
-                    .registerTypeAdapter(ExerciseCommand.class, new ExerciseCommandSerializer())
-                    .create();
+        new GsonBuilder()
+            .registerTypeAdapter(ExerciseCommand.class, new ExerciseCommandSerializer())
+            .registerTypeAdapter(Identifier.class, new IdentifierSerializer())
+            .registerTypeAdapter(Record.class, new RecordSerializer())
+            .create();
     return gson.toJson(o);
-  }
-
-  private String jsonFormat(String json) {
-    JsonParser parser = new JsonParser();
-    Gson gson = new GsonBuilder().setPrettyPrinting().create();
-
-    JsonElement el = parser.parse(json);
-    return gson.toJson(el); // done
   }
 }
