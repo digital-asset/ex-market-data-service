4c9c70fd087d49ba43c4ec1e81661cb57e1d6566
Richard Kapolnai <richard.kapolnai@digitalasset.com>
2020-02-10 11:18:14 +0100

    ERA-700 deserialize (#17)
    * SampleCurrentTimeDeserializer
    
    * fix
    
    * format
    
    * add GsonRegisteredAllDeserializers
    
    * remove deserializerbase
    
    * add test deserializeEmptyDataStream
    
    * add test for DataStream
    
    * simplify deser
    
    * remove println
    
    * format
    
    * ObservationValue deserialization
    
    * comment
    
    * format

---
+++ src/main/java/jsonapi/json/GsonRegisteredAllDeserializers.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2019, Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package jsonapi.json;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import da.refapps.marketdataservice.marketdatatypes.ObservationValue;
+import java.time.Instant;
+import jsonapi.gson.InstantDeserializer;
+import jsonapi.gson.ObservationValueDeserializer;
+
+public class GsonRegisteredAllDeserializers {
+
+  public static Gson gson() {
+    return new GsonBuilder()
+        .registerTypeAdapter(Instant.class, new InstantDeserializer())
+        .registerTypeAdapter(ObservationValue.class, new ObservationValueDeserializer())
+        .create();
+  }
+}
