eddac8c79e0c1e4d54cac8c01819846565a5111b
Richard Kapolnai <richard.kapolnai@digitalasset.com>
2020-02-10 23:33:40 +0100

    format

--- src/main/java/jsonapi/gson/IdentifierDeserializer.java
+++ src/main/java/jsonapi/gson/IdentifierDeserializer.java
@@ -9,11 +9,9 @@ import com.google.gson.JsonDeserializationContext;
 import com.google.gson.JsonDeserializer;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParseException;
-
 import java.lang.reflect.Type;
 
-public class IdentifierDeserializer
-    implements JsonDeserializer<Identifier> {
+public class IdentifierDeserializer implements JsonDeserializer<Identifier> {
 
   @Override
   public Identifier deserialize(
