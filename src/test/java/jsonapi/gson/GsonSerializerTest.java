568b48ee9253bd33f4dd4943972c31341088b37b
Tamás Kálcza <tamas.kalcza@digitalasset.com>
2020-02-12 15:49:38 +0100

    ERA-707 Moved test class to correct package.

--- src/test/java/jsonapi/gson/GsonSerializerTest.java
+++ src/test/java/jsonapi/gson/GsonSerializerTest.java
@@ -2,7 +2,7 @@
  * Copyright (c) 2019, Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
  * SPDX-License-Identifier: Apache-2.0
  */
-package jsonapi;
+package jsonapi.gson;
 
 import com.daml.ledger.javaapi.data.Date;
 import com.daml.ledger.javaapi.data.ExerciseCommand;
