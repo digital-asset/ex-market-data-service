3cf04bfd1e1629b30330a98bd1e031d901d99581
Tamás Kálcza <tamas.kalcza@digitalasset.com>
2020-02-12 16:15:14 +0100

    ERA-707 Added test for record serialization.

--- src/test/java/jsonapi/gson/RecordSerializerTest.java
+++ src/test/java/jsonapi/gson/RecordSerializerTest.java
@@ -7,10 +7,13 @@ package jsonapi.gson;
 import static org.junit.Assert.assertEquals;
 
 import com.daml.ledger.javaapi.data.Identifier;
+import com.daml.ledger.javaapi.data.Numeric;
 import com.daml.ledger.javaapi.data.Record;
+import com.daml.ledger.javaapi.data.Record.Field;
 import com.google.gson.Gson;
 import com.google.gson.JsonSerializer;
 import java.lang.reflect.Type;
+import java.math.BigDecimal;
 import org.junit.Test;
 
 public class RecordSerializerTest extends SerializerBaseTest<Record> {
@@ -27,6 +30,24 @@ public class RecordSerializerTest extends SerializerBaseTest<Record> {
   @Test
   public void identifierIsLeftOut() {
     Record record = new Record(new Identifier("p", "m", "e"));
+
+    Gson serializer = createSerializer();
+
+    assertEquals("{}", serializer.toJson(record));
+  }
+
+  @Test
+  public void fieldWithLabelIsSerialized() {
+    Record record = new Record(new Field("apple", new Numeric(BigDecimal.ONE)));
+
+    Gson serializer = createSerializer();
+
+    assertEquals("{\"apple\":{\"value\":1}}", serializer.toJson(record));
+  }
+
+  @Test
+  public void fieldWithoutLabelIsNotSerialized() {
+    Record record = new Record(new Field(new Numeric(BigDecimal.ONE)));
 
     Gson serializer = createSerializer();
 
