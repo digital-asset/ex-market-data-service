843057cd665f2a80e540fd0c5072eecd1c46518c
Richard Kapolnai <richard.kapolnai@digitalasset.com>
2020-02-12 12:31:51 +0100

    fix GsonDeserilizer

--- src/main/java/jsonapi/gson/GsonDeserializer.java
+++ src/main/java/jsonapi/gson/GsonDeserializer.java
@@ -4,25 +4,17 @@
  */
 package jsonapi.gson;
 
-import com.daml.ledger.javaapi.data.Template;
 import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import jsonapi.events.Event;
 import jsonapi.http.HttpResponse;
 import jsonapi.http.WebSocketResponse;
+import jsonapi.json.GsonRegisteredAllDeserializers;
 import jsonapi.json.JsonDeserializer;
 
 public class GsonDeserializer {
 
-  private Gson gson =
-      new GsonBuilder()
-          .registerTypeAdapter(WebSocketResponse.class, new InstantDeserializer())
-          .registerTypeAdapter(WebSocketResponse.class, new WebSocketResponseDeserializer())
-          .registerTypeAdapter(Event.class, new EventDeserializer())
-          .registerTypeAdapter(Template.class, new CreatedEventDeserializer())
-          .create();
+  private Gson gson = GsonRegisteredAllDeserializers.gson();
 
   private final JsonDeserializer<HttpResponse> httpResponseJsonDeserializer =
       s -> gson.fromJson(toReader(s), HttpResponse.class);
