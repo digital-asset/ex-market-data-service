97ff64d7ab3b0a595be97e51b4faf0970de9d1a6
Tamás Kálcza <tamas.kalcza@digitalasset.com>
2020-02-13 13:52:32 +0100

    ERA-707 Updated and fixed WebSocket deserializer for query response.

--- src/test/java/jsonapi/gson/WebSocketResponseDeserializerTest.java
+++ src/test/java/jsonapi/gson/WebSocketResponseDeserializerTest.java
@@ -12,8 +12,9 @@ import static org.junit.Assert.assertThat;
 import com.daml.ledger.javaapi.data.Identifier;
 import com.google.gson.Gson;
 import com.google.gson.JsonDeserializer;
-import da.refapps.marketdataservice.roles.OperatorRole;
+import da.timeservice.timeservice.CurrentTime;
 import java.lang.reflect.Type;
+import java.time.Instant;
 import jsonapi.events.CreatedEvent;
 import jsonapi.events.Event;
 import jsonapi.http.EventHolder;
@@ -23,29 +24,37 @@ import org.junit.Test;
 public class WebSocketResponseDeserializerTest extends DeserializerBaseTest<WebSocketResponse> {
 
   @Test
-  public void TodoIsThereSuchJsonInput() {
-    String tid = new GsonSerializer().apply(OperatorRole.TEMPLATE_ID);
+  public void deserializeQueryResponse() {
+    String templateId = new GsonSerializer().apply(CurrentTime.TEMPLATE_ID);
     String json =
-        "[ \n"
-            + "   { \n"
-            + "      \"created\":{ \n"
-            + "         \"observers\":[],\n"
-            + "         \"agreementText\":\"\",\n"
-            + "         \"payload\":{ \n"
-            + "            \"operator\":\"Operator\"\n"
-            + "         },\n"
-            + "         \"signatories\":[ \n"
-            + "            \"Operator\"\n"
-            + "         ],\n"
-            + "         \"key\":\"Operator\",\n"
-            + "         \"contractId\":\"#11:0\",\n"
-            + "         \"templateId\":"
-            + tid
+        "{\n"
+            + "  \"events\": [\n"
+            + "    {\n"
+            + "      \"created\": {\n"
+            + "        \"observers\": [],\n"
+            + "        \"agreementText\": \"\",\n"
+            + "        \"payload\": {\n"
+            + "          \"operator\": \"Operator\",\n"
+            + "          \"currentTime\": \"2020-02-13T10:29:57.413838Z\",\n"
+            + "          \"observers\": []\n"
+            + "        },\n"
+            + "        \"signatories\": [\n"
+            + "          \"Operator\"\n"
+            + "        ],\n"
+            + "        \"key\": \"Operator\",\n"
+            + "        \"contractId\": \"#12:0\",\n"
+            + "        \"templateId\": "
+            + templateId
             + "\n"
-            + "      }\n"
-            + "   }\n"
-            + "]";
+            + "      },\n"
+            + "      \"matchedQueries\": [\n"
+            + "        0\n"
+            + "      ]\n"
+            + "    }\n"
+            + "  ]\n"
+            + "}";
     registerDeserializer(Identifier.class, new IdentifierDeserializer());
+    registerDeserializer(Instant.class, new InstantDeserializer());
     registerDeserializer(Event.class, new EventDeserializer());
     registerDeserializer(CreatedEvent.class, new CreatedEventDeserializer());
     registerDeserializer(EventHolder.class, new EventHolderDeserializer());
@@ -55,42 +64,6 @@ public class WebSocketResponseDeserializerTest extends DeserializerBaseTest<WebS
     WebSocketResponse result = deserializer.fromJson(json, getDeserializedClass());
     assertThat(result.getEvents().size(), is(1));
     assertThat(result.getEvents(), everyItem(instanceOf(CreatedEvent.class)));
-  }
-
-  @Test
-  public void search() {
-    String serializedWebSocketResponse =
-        "{ \n"
-            + "   \"events\":[ \n"
-            + "      { \n"
-            + "         \"created\":{ \n"
-            + "            \"observers\":[ \n"
-            + "\n"
-            + "            ],\n"
-            + "            \"agreementText\":\"\",\n"
-            + "            \"payload\":{ \n"
-            + "               \"operator\":\"Operator\",\n"
-            + "               \"currentTime\":\"2020-02-13T10:29:57.413838Z\",\n"
-            + "               \"observers\":[ \n"
-            + "\n"
-            + "               ]\n"
-            + "            },\n"
-            + "            \"signatories\":[ \n"
-            + "               \"Operator\"\n"
-            + "            ],\n"
-            + "            \"key\":\"Operator\",\n"
-            + "            \"contractId\":\"#12:0\",\n"
-            + "            \"templateId\":\"230a15b6240603917c18612a7dcb83a7040ab1cf8d498bb4b523b5de03659f58:DA.TimeService.TimeService:CurrentTime\"\n"
-            + "         },\n"
-            + "         \"matchedQueries\":[ \n"
-            + "            0\n"
-            + "         ]\n"
-            + "      }\n"
-            + "   ]\n"
-            + "}";
-    WebSocketResponse deserializedWebSocketResponse =
-        GsonRegisteredAllDeserializers.gson()
-            .fromJson(serializedWebSocketResponse, WebSocketResponse.class);
   }
 
   @Override
