fb05a599e1c6471a3b0ec9772997885ed790534c
Tamás Kálcza <tamas.kalcza@digitalasset.com>
2020-02-13 13:39:45 +0100

    ERA-707 Reworked WebSocket deserializer test to use base test.

--- src/test/java/jsonapi/gson/WebSocketResponseDeserializerTest.java
+++ src/test/java/jsonapi/gson/WebSocketResponseDeserializerTest.java
@@ -4,23 +4,28 @@
  */
 package jsonapi.gson;
 
+import static org.hamcrest.CoreMatchers.everyItem;
 import static org.hamcrest.CoreMatchers.instanceOf;
-import static org.junit.Assert.assertEquals;
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
-import com.google.common.collect.Iterables;
+import com.daml.ledger.javaapi.data.Identifier;
+import com.google.gson.Gson;
+import com.google.gson.JsonDeserializer;
 import da.refapps.marketdataservice.roles.OperatorRole;
+import java.lang.reflect.Type;
 import jsonapi.events.CreatedEvent;
 import jsonapi.events.Event;
+import jsonapi.http.EventHolder;
 import jsonapi.http.WebSocketResponse;
 import org.junit.Test;
 
-public class WebSocketResponseDeserializerTest {
+public class WebSocketResponseDeserializerTest extends DeserializerBaseTest<WebSocketResponse> {
 
   @Test
   public void TodoIsThereSuchJsonInput() {
     String tid = new GsonSerializer().apply(OperatorRole.TEMPLATE_ID);
-    String serializedWebSocketResponse =
+    String json =
         "[ \n"
             + "   { \n"
             + "      \"created\":{ \n"
@@ -40,12 +45,16 @@ public class WebSocketResponseDeserializerTest {
             + "      }\n"
             + "   }\n"
             + "]";
-    WebSocketResponse deserializedWebSocketResponse =
-        GsonRegisteredAllDeserializers.gson()
-            .fromJson(serializedWebSocketResponse, WebSocketResponse.class);
-    assertEquals(deserializedWebSocketResponse.getEvents().size(), 1);
-    Event event = Iterables.getOnlyElement(deserializedWebSocketResponse.getEvents());
-    assertThat(event, instanceOf(CreatedEvent.class));
+    registerDeserializer(Identifier.class, new IdentifierDeserializer());
+    registerDeserializer(Event.class, new EventDeserializer());
+    registerDeserializer(CreatedEvent.class, new CreatedEventDeserializer());
+    registerDeserializer(EventHolder.class, new EventHolderDeserializer());
+
+    Gson deserializer = createDeserializer();
+
+    WebSocketResponse result = deserializer.fromJson(json, getDeserializedClass());
+    assertThat(result.getEvents().size(), is(1));
+    assertThat(result.getEvents(), everyItem(instanceOf(CreatedEvent.class)));
   }
 
   @Test
@@ -82,5 +91,15 @@ public class WebSocketResponseDeserializerTest {
     WebSocketResponse deserializedWebSocketResponse =
         GsonRegisteredAllDeserializers.gson()
             .fromJson(serializedWebSocketResponse, WebSocketResponse.class);
+  }
+
+  @Override
+  protected Type getDeserializedClass() {
+    return WebSocketResponse.class;
+  }
+
+  @Override
+  protected JsonDeserializer<WebSocketResponse> getClassDeserializer() {
+    return new WebSocketResponseDeserializer();
   }
 }
