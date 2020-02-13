c4c942dbae6698dd7aabc743925d47edc8389e63
Tamás Kálcza <tamas.kalcza@digitalasset.com>
2020-02-13 11:37:23 +0100

    ERA-707 Helpers deserializers can be registered in unit tests.

--- src/test/java/jsonapi/gson/DeserializerBaseTest.java
+++ src/test/java/jsonapi/gson/DeserializerBaseTest.java
@@ -7,14 +7,29 @@ package jsonapi.gson;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonSerializer;
 import java.lang.reflect.Type;
+import java.util.HashMap;
+import java.util.Map;
 
 public abstract class DeserializerBaseTest<T> {
 
+  private Map<Type, JsonSerializer<?>> serializers = new HashMap<>();
+
   protected Gson createDeserializer() {
-    return new GsonBuilder()
-        .registerTypeAdapter(getDeserializedClass(), getClassDeserializer())
+    GsonBuilder builderWithDefaultSerializer =
+        new GsonBuilder().registerTypeAdapter(getDeserializedClass(), getClassDeserializer());
+    return serializers.entrySet().stream()
+        .reduce(
+            builderWithDefaultSerializer,
+            (builder, serializer) ->
+                builder.registerTypeAdapter(serializer.getKey(), serializer.getValue()),
+            (x, y) -> x)
         .create();
+  }
+
+  protected <U> void registerSerializer(Type type, JsonSerializer<U> serializer) {
+    serializers.put(type, serializer);
   }
 
   protected abstract Type getDeserializedClass();
