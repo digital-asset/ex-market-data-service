5e708b83ec8cacb83261bc9986cf950a6f922c1d
Tamás Kálcza <tamas.kalcza@digitalasset.com>
2020-02-13 15:00:43 +0100

    ERA-707 Added left out deserializer.

---
+++ src/main/java/jsonapi/gson/CreateResultDeserializer.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2019, Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package jsonapi.gson;
+
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
+import java.lang.reflect.Type;
+import jsonapi.events.CreatedEvent;
+import jsonapi.http.HttpResponse.CreateResult;
+
+public class CreateResultDeserializer implements JsonDeserializer<CreateResult> {
+
+  @Override
+  public CreateResult deserialize(
+      JsonElement jsonElement, Type type, JsonDeserializationContext jsonDeserializationContext)
+      throws JsonParseException {
+    CreatedEvent createdEvent =
+        jsonDeserializationContext.deserialize(jsonElement, CreatedEvent.class);
+    return new CreateResult(createdEvent);
+  }
+}
