f38bfff3f28a0b7cdb1d529b5c5f353afb59b93b
Richard Kapolnai <richard.kapolnai@digitalasset.com>
2020-02-21 16:31:23 +0100

    format

--- src/test/java/jsonapi/JsonLedgerClientTest.java
+++ src/test/java/jsonapi/JsonLedgerClientTest.java
@@ -94,7 +94,7 @@ public class JsonLedgerClientTest {
   @Test
   public void getActiveContractsThrowsForWebSocketWarningAndIncludesMessage() {
     WebSocketClientGivingError webSocketClient =
-            new WebSocketClientGivingError(new WebSocketResponse(null, null, "some warning"));
+        new WebSocketClientGivingError(new WebSocketResponse(null, null, "some warning"));
     JsonLedgerClient ledger = new JsonLedgerClient(null, webSocketClient, jsonSerializer, api);
     exceptionRule.expectMessage("some warning");
     Flowable<ActiveContractSet> activeContracts = ledger.getActiveContracts(null);
--- src/test/java/jsonapi/http/WebSocketResponseTest.java
+++ src/test/java/jsonapi/http/WebSocketResponseTest.java
@@ -8,24 +8,19 @@ import static org.hamcrest.CoreMatchers.everyItem;
 import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
-import static org.junit.Assert.assertTrue;
 
 import java.util.Collection;
 import java.util.Collections;
-import java.util.List;
-
 import jsonapi.events.CreatedEvent;
 import jsonapi.events.Event;
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
 
 public class WebSocketResponseTest {
   @Test
   public void toEvents() {
-    Collection<EventHolder> events = Collections.singletonList(new CreatedEventHolder(new CreatedEvent(null, null, null)));
-    Collection<Event> actual =
-        new WebSocketResponse(events, null, null).getEvents();
+    Collection<EventHolder> events =
+        Collections.singletonList(new CreatedEventHolder(new CreatedEvent(null, null, null)));
+    Collection<Event> actual = new WebSocketResponse(events, null, null).getEvents();
     assertEquals(1, actual.size());
     assertThat(actual, everyItem(instanceOf(CreatedEvent.class)));
   }
